aad6c9af2d841eb59d76b936373c97924a9507b2
pashaiva <pashaiva@mail.ru>
2015-04-02 18:20:35 +0300

    test

--- psub.Shared/LanguageConstants.cs
+++ psub.Shared/LanguageConstants.cs
@@ -2,7 +2,7 @@
 {
    public static class LanguageConstants
    {
-       public const string Text = "Текст";
+       public const string Text = "Текст ";
        public const string Author = "Автор";
        public const string Comment = "Комментарий";
        public const string Comments = "Комментарии";
